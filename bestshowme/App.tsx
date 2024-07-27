a18d81c2b5d8b97170a2096c8876a72d1f599099
Marilise Morona <marymorona@gmail.com>
2024-07-26 21:29:50 -0300

    [RN][Feat-03]: configure theme project

--- bestshowme/App.tsx
+++ bestshowme/App.tsx
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/utils/styles/theme';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold
+  })
+
   return (
+    <ThemeProvider theme={theme}>
     <View style={styles.container}>
-      <Text>Startando o APP.</Text>
+      <Text style={styles.text}>Startando o APP.</Text>
       <StatusBar style="auto" />
     </View>
+    </ThemeProvider>
   );
 }
 
@@ -17,5 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: theme.colors.pink,
+    fontFamily: theme.fonts.bold
   },
 });
---
+++ bestshowme/src/utils/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+      background: '#ffffff',
+      pink: '#ef3b65',
+      blue: '#96cbe8',
+      purple: '#97a0ec',
+      purpleLight: '#a4adf1',
+      text: '#000000',
+    },
+  
+    fonts: {
+      regular: 'Inter_400Regular',
+      medium: 'Inter_500Medium',
+      bold: 'Inter_700Bold',
+    }
+  }
